5919bb2998e9646cbfe361c69ce1b22a822ef2ee
Luncert <2725115515@qq.com>
2024-02-05 22:09:09 +0800

    fix contraption anchor rendering

--- src/main/java/com/luncert/vibotech/content/TransportMachineContraption.java
+++ src/main/java/com/luncert/vibotech/content/TransportMachineContraption.java
@@ -5,12 +5,18 @@ import com.mojang.logging.LogUtils;
 import com.simibubi.create.content.contraptions.AssemblyException;
 import com.simibubi.create.content.contraptions.Contraption;
 import com.simibubi.create.content.contraptions.ContraptionType;
+import com.simibubi.create.content.contraptions.render.ContraptionLighter;
+import com.simibubi.create.content.contraptions.render.NonStationaryLighter;
 import java.util.Queue;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.levelgen.structure.templatesystem.StructureTemplate.StructureBlockInfo;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import org.apache.commons.lang3.tuple.Pair;
 import org.slf4j.Logger;
 
@@ -49,11 +55,6 @@ public class TransportMachineContraption extends Contraption {
   }
 
   @Override
-  public boolean canBeStabilized(Direction direction, BlockPos blockPos) {
-    return false;
-  }
-
-  @Override
   protected Pair<StructureBlockInfo, BlockEntity> capture(Level world, BlockPos pos) {
     Pair<StructureBlockInfo, BlockEntity> pair = super.capture(world, pos);
     StructureBlockInfo capture = pair.getKey();
@@ -63,5 +64,26 @@ public class TransportMachineContraption extends Contraption {
 
     // replace assemble station with anchor block
     return Pair.of(new StructureBlockInfo(pos, AssembleStationBlock.createAnchor(capture.state()), null), pair.getValue());
+  }
+
+  @Override
+  protected boolean customBlockPlacement(LevelAccessor world, BlockPos pos, BlockState state) {
+    return AllBlocks.TRANSPORT_MACHINE_ANCHOR.has(state);
+  }
+
+  @Override
+  protected boolean customBlockRemoval(LevelAccessor world, BlockPos pos, BlockState state) {
+    return AllBlocks.TRANSPORT_MACHINE_ANCHOR.has(state);
+  }
+
+  @Override
+  public boolean canBeStabilized(Direction direction, BlockPos blockPos) {
+    return false;
+  }
+
+  @Override
+  @OnlyIn(Dist.CLIENT)
+  public ContraptionLighter<?> makeLighter() {
+    return new NonStationaryLighter<>(this);
   }
 }
