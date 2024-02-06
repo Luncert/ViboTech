99095f95d88081fea79f3593117c41452bbb71aa
Luncert <2725115515@qq.com>
2024-02-07 05:27:28 +0800

    working on item nbt, block mining and recipes

--- src/main/java/com/luncert/vibotech/index/AllBlocks.java
+++ src/main/java/com/luncert/vibotech/index/AllBlocks.java
@@ -1,14 +1,17 @@
 package com.luncert.vibotech.index;
 
 import static com.luncert.vibotech.ViboTechMod.REGISTRATE;
-import static com.simibubi.create.foundation.data.ModelGen.customItemModel;
 import static com.simibubi.create.foundation.data.TagGen.pickaxeOnly;
 
 import com.luncert.vibotech.content.AssembleStationBlock;
+import com.luncert.vibotech.content.AssembleStationBlockEntity;
 import com.luncert.vibotech.content.AssembleStationItem;
 import com.luncert.vibotech.content.TransportMachineAnchorBlock;
+import com.simibubi.create.AllTags;
 import com.simibubi.create.foundation.data.SharedProperties;
 import com.tterrag.registrate.util.entry.BlockEntry;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.MapColor;
 
 public class AllBlocks {
@@ -19,9 +22,12 @@ public class AllBlocks {
 
   public static final BlockEntry<AssembleStationBlock> ASSEMBLE_STATION =
       REGISTRATE.block("assemble_station", AssembleStationBlock::new)
-          .initialProperties(SharedProperties::softMetal)
-          .properties(p -> p.mapColor(MapColor.COLOR_GRAY))
+          .initialProperties(SharedProperties::stone)
+          .properties(p -> p.mapColor(MapColor.COLOR_GRAY)
+              .requiresCorrectToolForDrops())
+          .properties(BlockBehaviour.Properties::noOcclusion)
           .transform(pickaxeOnly())
+          .tag(AllTags.AllBlockTags.SAFE_NBT.tag)
           .blockstate((ctx, prov) -> prov.horizontalBlock(ctx.getEntry(), prov.models()
               .getExistingFile(ctx.getId()), 0))
           .item(AssembleStationItem::new)
