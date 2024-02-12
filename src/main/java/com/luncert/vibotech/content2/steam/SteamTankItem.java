c7e6720c184dcc9eeb4f8fc4bb115eca289d7bf9
Luncert <2725115515@qq.com>
2024-02-12 11:54:27 +0800

    working on steam tank item

--- src/main/java/com/luncert/vibotech/content2/steam/SteamTankItem.java
+++ src/main/java/com/luncert/vibotech/content2/steam/SteamTankItem.java
@@ -1,23 +1,35 @@
 package com.luncert.vibotech.content2.steam;
 
-import com.luncert.vibotech.index.AllBlocks;
-import net.minecraft.world.item.BlockItem;
+import com.luncert.vibotech.content2.gastank.GasTankBlockEntity;
+import com.luncert.vibotech.index.AllFluids;
+import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.context.BlockPlaceContext;
-import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fluids.capability.IFluidHandler;
+import org.slf4j.Logger;
 
-public class SteamTankItem extends BlockItem {
+public class SteamTankItem extends Item {
+
+   private static final Logger LOGGER = LogUtils.getLogger();
 
   public SteamTankItem(Properties properties) {
-    super(AllBlocks.GAS_TANK.get(), properties);
+    super(properties);
   }
 
-  @Override
-  protected boolean placeBlock(BlockPlaceContext context, BlockState state) {
-    boolean ok = super.placeBlock(context, state);
-    if (ok) {
-      BlockEntity blockEntity = context.getLevel().getBlockEntity(context.getClickedPos());
-    }
-    return ok;
-  }
+  //@Override
+  //protected boolean placeBlock(BlockPlaceContext context, BlockState state) {
+  //  boolean ok = super.placeBlock(context, state);
+  //  if (ok) {
+  //    LOGGER.info("ok");
+  //    GasTankBlockEntity be = (GasTankBlockEntity) context.getLevel()
+  //        .getBlockEntity(context.getClickedPos());
+  //    FluidStack content = new FluidStack(AllFluids.STEAM.get()
+  //        .getSource(), 2000);
+  //    LOGGER.info("{}", be.getTankInventory().fill(content, IFluidHandler.FluidAction.SIMULATE));
+  //    be.getTankInventory().fill(content, IFluidHandler.FluidAction.EXECUTE);
+  //  }
+  //  return ok;
+  //}
 }
