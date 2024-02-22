96fecbebac4b7261f898d80dde7dbcec746fafa4
Luncert <2725115515@qq.com>
2024-02-22 09:41:44 +0800

    add PhotovoltaicPanelBlock

--- src/main/java/com/luncert/vibotech/content/photovoltaic/PhotovoltaicPanelBlock.java
+++ src/main/java/com/luncert/vibotech/content/photovoltaic/PhotovoltaicPanelBlock.java
@@ -1,12 +1,24 @@
 package com.luncert.vibotech.content.photovoltaic;
 
+import com.google.common.collect.ImmutableMap;
 import com.luncert.vibotech.index.AllBlockEntityTypes;
+import com.luncert.vibotech.index.AllShapes;
 import com.simibubi.create.content.equipment.wrench.IWrenchable;
 import com.simibubi.create.foundation.block.IBE;
+import java.util.function.Function;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class PhotovoltaicPanelBlock extends Block implements IWrenchable, IBE<PhotovoltaicPanelBlockEntity> {
+
+  public static final VoxelShape SHAPE = AllShapes
+      .shape(0, 0, 0, 16, 3, 16)
+      .build();
 
   public PhotovoltaicPanelBlock(Properties properties) {
     super(properties);
@@ -20,5 +32,10 @@ public class PhotovoltaicPanelBlock extends Block implements IWrenchable, IBE<Ph
   @Override
   public BlockEntityType<? extends PhotovoltaicPanelBlockEntity> getBlockEntityType() {
     return AllBlockEntityTypes.PHOTOVOLTAIC_PANEL.get();
+  }
+
+  @Override
+  public VoxelShape getShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
+    return SHAPE;
   }
 }
--- src/main/java/com/luncert/vibotech/content/photovoltaic/PhotovoltaicPanelComponent.java
+++ src/main/java/com/luncert/vibotech/content/photovoltaic/PhotovoltaicPanelComponent.java
@@ -1,6 +1,7 @@
 package com.luncert.vibotech.content.photovoltaic;
 
 import com.luncert.vibotech.compat.vibotech.BaseViboComponent;
+import com.luncert.vibotech.compat.vibotech.ViboComponentTickContext;
 import com.luncert.vibotech.compat.vibotech.ViboComponentType;
 
 public class PhotovoltaicPanelComponent extends BaseViboComponent {
@@ -8,5 +9,14 @@ public class PhotovoltaicPanelComponent extends BaseViboComponent {
   @Override
   public ViboComponentType getComponentType() {
     return ViboComponentType.PHOTOVOLTAIC_PANEL;
+  }
+
+  @Override
+  public void tickComponent(ViboComponentTickContext context) {
+    if (accessor.world.dimensionType().hasSkyLight()) {
+      getEnergyAccessor().ifPresent(energyStorage -> {
+        energyStorage.receiveEnergy(10, false);
+      });
+    }
   }
 }
