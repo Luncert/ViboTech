2b0996bd0b31f4fa3f7855b789e007b2c53f8465
Luncert <2725115515@qq.com>
2024-02-23 18:31:07 +0800

    working on control seat

--- src/main/java/com/luncert/vibotech/content/controlseat/ControlSeatComponent.java
+++ src/main/java/com/luncert/vibotech/content/controlseat/ControlSeatComponent.java
@@ -3,7 +3,6 @@ package com.luncert.vibotech.content.controlseat;
 import com.luncert.vibotech.compat.vibotech.BaseViboComponent;
 import com.luncert.vibotech.compat.vibotech.ViboComponentType;
 import com.mojang.blaze3d.platform.InputConstants;
-import com.mojang.logging.LogUtils;
 import dan200.computercraft.api.lua.LuaFunction;
 import java.util.Collection;
 import java.util.Collections;
@@ -12,11 +11,9 @@ import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 import net.minecraft.world.level.LevelAccessor;
-import org.slf4j.Logger;
 
 public class ControlSeatComponent extends BaseViboComponent {
 
-  private static final Logger LOGGER = LogUtils.getLogger();
   public static Set<InputConstants.Key> receivedInputs = Collections.emptySet();
   static final int TIMEOUT = 30;
 
@@ -39,7 +36,6 @@ public class ControlSeatComponent extends BaseViboComponent {
                                     UUID uniqueID,
                                     Collection<InputConstants.Key> activatedButtons,
                                     boolean pressed) {
-    LOGGER.info("{}", activatedButtons);
     if (pressed) {
       receivedInputs = new HashSet<>(activatedButtons);
     } else {
