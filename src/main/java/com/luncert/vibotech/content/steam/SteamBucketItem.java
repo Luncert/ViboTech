4a621cf4432414d5e702c71baef4bad98758640d
Luncert <2725115515@qq.com>
2024-03-05 15:48:53 +0800

    working on air compressor

--- src/main/java/com/luncert/vibotech/content/steam/SteamBucketItem.java
+++ src/main/java/com/luncert/vibotech/content/steam/SteamBucketItem.java
@@ -1,11 +1,11 @@
-package com.luncert.vibotech.content.steam;
-
-import com.luncert.vibotech.index.AllFluids;
-import net.minecraft.world.item.BucketItem;
-
-public class SteamBucketItem extends BucketItem {
-
-  public SteamBucketItem(Properties properties) {
-    super(AllFluids.STEAM, properties);
-  }
-}
+//package com.luncert.vibotech.content.steam;
+//
+//import com.luncert.vibotech.index.AllFluids;
+//import net.minecraft.world.item.BucketItem;
+//
+//public class SteamBucketItem extends BucketItem {
+//
+//  public SteamBucketItem(Properties properties) {
+//    super(AllFluids.AIR, properties);
+//  }
+//}
